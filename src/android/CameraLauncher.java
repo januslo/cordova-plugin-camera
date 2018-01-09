c23846466906741a6432097333c9f59d4c9cbd17
janus j k lu <januslo6211@icloud.com>
2018-01-09 16:53:33 +0800

    Resovle conflicts

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -702,13 +702,8 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
                 this.callbackContext.success(uriString);
             } else {
                 // If we don't have a valid image so quit.
-<<<<<<< HEAD
                 if (!("image/jpg".equalsIgnoreCase(mimeType) || "image/jpeg".equalsIgnoreCase(mimeType) || "image/png".equalsIgnoreCase(mimeType))) {
                     Log.d(LOG_TAG, "I either have a null image path or bitmap");
-=======
-                if (!("image/jpeg".equalsIgnoreCase(mimeType) || "image/png".equalsIgnoreCase(mimeType))) {
-                    LOG.d(LOG_TAG, "I either have a null image path or bitmap");
->>>>>>> apache/master
                     this.failPicture("Unable to retrieve path to picture!");
                     return;
                 }
